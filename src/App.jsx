9683bb3bc712174f2a3881e4082f3a9c2074e127
Wasim Rehman <100414282+WasimRehmn@users.noreply.github.com>
2022-04-04 22:07:52 +0530

    start working on product page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Products } from "./Components/Products";
+
+function App() {
+    return (
+        <div className="App">
+            <Products />
+        </div>
+    );
+}
+
+export default App;
