c30393255909e9df95fbdcbebfbfb85b933c03c2
ansari-saif <saifhussainansari6@gmail.com>
2022-04-09 02:22:06 +0530

    changes

--- src/Components/CheckOut/Login.jsx
+++ src/Components/CheckOut/Login.jsx
@@ -45,27 +45,28 @@ const Login = () => {
                     </div>
                 </div>
                 {/* sidebar right */}
+
                 <div className="col-md-4">
                     <div className="card">
                         <div className='sideHeading'>1 Items in your Bag
                             <span>Edit</span>
                         </div>
-                        <div className="products row">
-                            <img src="/product1.jpg" alt="product" className='col-md-4' />
+                        {cartProducts.map((e,i) => <div key={i} className="products row">
+                            <img src={e.image1} alt="product" className='col-md-4' />
                             <div className="col-md-8">
-                                <div className="productNmae">Ponds Bright Beauty Vitamin C Face Serum Infused With Lemon .</div>
+                                <div className="productNmae">{e.card_title}</div>
                                 <div className="quantity">3ml</div>
                                 <hr />
                                 <div className="productFooter row">
                                     <div className="qty col-md-6">Qty : 1</div>
                                     <div className="price col-md-6">
-                                        <strike>499</strike>
+                                        <strike>{e.off_price}</strike>
                                         &nbsp;
-                                        <span>424</span>
+                                        <span>{e.price}</span>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div>)}
 
                         <div className="bill">
                             <div className="content">
