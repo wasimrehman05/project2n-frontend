1cf844bf2c40f306dde359f9611317bfc887a9d8
ansari-saif <saifhussainansari6@gmail.com>
2022-04-08 14:54:48 +0530

    payment Tabs

--- src/Components/CheckOut/AddressCheckout.jsx
+++ src/Components/CheckOut/AddressCheckout.jsx
@@ -3,42 +3,33 @@ import { Div } from "../StyledComponents/NykaaStyles";
 import Nav from './Nav'
 import SideBarRight from "./SideBarRight";
 const Address = () => {
-    function tabHandler(e) {
-        const lis = e.currentTarget.parentNode.childNodes;
-        lis.forEach(e => e.classList.remove("active"))
-        e.currentTarget.classList.add("active");
-    }
+
     return (
         <Div >
             <Nav active="address" />
             <div className="row">
                 <div className="col-md-4">
                     <ul className='tabs'>
-                        <li onClick={tabHandler} className='active'>Content 1</li>
-                        <li onClick={tabHandler} >Content 2</li>
-                        <li onClick={tabHandler} >Content 3</li>
-                        <li onClick={tabHandler} >Content 4</li>
+                        <li className='active'>New Address</li>
+
                     </ul>
                 </div>
                 <div className="col-md-4">
                     <div className="card">
-                        <h5>Address or Register</h5>
+                        <h5>New Address</h5>
                         <hr />
-                        <div className="subHeading">
-                            <div><i className="fa fa-check"></i> Get Reward Points on new registration</div>
-                            <div><i className="fa fa-check"></i> Redeem your Coupons  Reward Points</div>
+                        <div className="address">
+                            <form>
+                                <input type="country" name="country" placeholder="Country" />
+                                <input type="country" name="name" placeholder="Name" />
+                                <input type="country" name="email" placeholder="Email" />
+                                <input type="country" name="email" placeholder="Email" />
+                                <input type="country" name="phone" placeholder="Phone" />
+                                <input type="country" name="postalCode" placeholder="Postal Code" />
+                                <textarea name="address" id="address" cols="30" rows="5" placeholder="Address"></textarea>
+                            </form>
+                            <div className="button">SHIP TO THIS ADDRESS  &gt;</div>
                         </div>
-                        <span className="text-muted">Continue with Social login</span>
-                        <div className="logos">
-                            <img src="/facebook.png" alt="fb" />
-                            <img src="/google.png" alt="fb" />
-                        </div>
-                        <div className="button">ENTER PHONE NUMBER OR EMAIL </div>
-                        <div className="subHeadingText">Checkout as guest</div>
-                        <div className="bottomText">
-                            I do not want any more benefits from Nykaa
-                        </div>
-                        <div className="button2">CONTINUE AS GUEST &gt;</div>
                     </div>
                 </div>
                 <SideBarRight />
--- src/Components/CheckOut/Payment.jsx
+++ src/Components/CheckOut/Payment.jsx
@@ -1,47 +1,73 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Div } from "../StyledComponents/NykaaStyles";
 import Nav from './Nav'
 import SideBarRight from "./SideBarRight";
 
 const Payment = () => {
+  const ref = useRef();
+  function tabHandler(e) {
+    const lis = e.currentTarget.parentNode.childNodes;
+    lis.forEach(e => e.classList.remove("active"))
+    const elm = e.currentTarget.className;
+    ref.current.childNodes.forEach(e => e.style.display = 'none');
+    ref.current.querySelector("#" + elm).style.display = 'block';
+    e.currentTarget.classList.add("active");
+  }
   return (
     <Div>
       <Nav active="payment" />
       <div class="notification">
-              <span class="noti-icon">i</span>
-              <span>
-                Please use a digital payment method & help us ensure contactless
-                delivery for your safety
-              </span>
-            </div>
+        <span class="noti-icon">i</span>
+        <span>
+          Please use a digital payment method & help us ensure contactless
+          delivery for your safety
+        </span>
+      </div>
       <div className="row">
         <div className="col-md-4">
           <ul className='tabs'>
-            <li className='active'>payment/Register</li>
-
+            <li onClick={tabHandler} className="credit active">Credit/Dbit Card</li>
+            <li onClick={tabHandler} className="upi">UPI</li>
+            <li onClick={tabHandler} className="google">Google Pay</li>
+            <li onClick={tabHandler} className="net">Net Banking</li>
+            <li onClick={tabHandler} className="mobile">Mobile Wallets</li>
+            <li onClick={tabHandler} className="gift">Gift Card</li>
           </ul>
         </div>
-        <div className="col-md-4">
-          <div className="card">
-            <h5>payment or Register</h5>
-          
-            <hr />
-            <div className="subHeading">
-              <div><i className="fa fa-check"></i> Get Reward Points on new registration</div>
-              <div><i className="fa fa-check"></i> Redeem your Coupons  Reward Points</div>
+        <div className="col-md-4 payment" ref={ref}>
+          <div id="credit" className="card active">
+            <div>
+              <h6>Credit/Debit Card tabsContent</h6>
             </div>
-            <span className="text-muted">Continue with Social login</span>
-            <div className="logos">
-              <img src="/facebook.png" alt="fb" />
-              <img src="/google.png" alt="fb" />
-            </div>
-            <div className="button">ENTER PHONE NUMBER OR EMAIL </div>
-            <div className="subHeadingText">Checkout as guest</div>
-            <div className="bottomText">
-              I do not want any more benefits from Nykaa
-            </div>
-            <div className="button2">CONTINUE AS GUEST &gt;</div>
           </div>
+          <div id="upi" className="card ">
+            <div>
+              <h6>BHIM UPI</h6>
+            </div>
+          </div>
+          <div id="google" className="card ">
+            <div>
+              <h6>Google Pay</h6>
+            </div>
+          </div>
+          <div id="net" className="card ">
+            <div>
+              <h6>Net Banking</h6>
+            </div>
+          </div>
+
+          <div id="mobile" className="card ">
+            <div>
+              <h6>Mobile Wallets</h6>
+            </div>
+          </div>
+
+          <div id="gift" className="card ">
+            <div>
+              <h6>Redeem Gift Card</h6>
+            </div>
+          </div>
+
         </div>
         <SideBarRight />
 
