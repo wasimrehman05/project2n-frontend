7d5e93848b280cce30941e5010f299d9d4a5bdd7
Wasim Rehman <iam_wasim@outlook.com>
2022-04-08 21:06:59 +0530

    Delete Home.jsx

--- src/Components/Home.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// import { Link } from "react-router-dom";
-// import { Link, useNavigate } from "react-router-dom";
-import { Cart } from "./Cart";
-
-export const Home = () => {
-    return <div style={{ marginTop: "15vh", width: "100%" }}></div>;
-};
