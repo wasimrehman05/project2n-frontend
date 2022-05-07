c26e0d221a018646f34090d308a2f461ea6bdaed
Wasim Rehman <100414282+WasimRehmn@users.noreply.github.com>
2022-05-07 10:41:09 +0530

    "Backend Connected"

---
+++ src/Components/Login/passwordGen.js
@@ -0,0 +1,15 @@
+const genPassword = () => {
+    var chars =
+        "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 12;
+    var password = "";
+    for (var i = 0; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber + 1);
+    }
+    return password;
+};
+
+module.exports = {
+    genPassword,
+};
