94b36e0aa30daba79e4e5439360bfcd1c3fe5a17
ansari-saif <saifhussainansari6@gmail.com>
2022-06-05 22:40:04 +0530

    bugfixes

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
-export const API_KEY = "https://nykaa-backend.herokuapp.com";
+export const API_KEY = "http://localhost:3006";
+// export const API_KEY = "http://nykaa-backend.herokuapp.com";
 export const CL_ID =
     "323093858656-5hogei86qaptjgggq7k3dnlj2m8b55am.apps.googleusercontent.com";
