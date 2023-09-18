6731171f983c245274b495600f66a52927ca6ea5
Wasim Rehman <wasim.rehman@anakin.company>
2023-09-19 05:03:19 +0530

    updated backend url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API_KEY = "http://localhost:3006";
+export const API_KEY = "https://api-project2n.cyclic.cloud";
 export const CL_ID =
     "323093858656-5hogei86qaptjgggq7k3dnlj2m8b55am.apps.googleusercontent.com";
